5907e1f781efdb3a5824c5af611912e0246a25c2
wangxu <martin.wang3@nio.com>
2025-06-05 11:31:57 +0800

    link dynamic library and include folder

--- external/buildSDL3.sh
+++ external/buildSDL3.sh
@@ -4,7 +4,7 @@ set -e
 OS_NAME="$(uname)"
 
 INSTALL_DIR="$(pwd)/../thirdparty/sdl3/lib"
-SDL_DIR="$(pwd)/SDL" # 你应将 SDL3 源码放在这个目录
+SDL_DIR="$(pwd)/SDL"
 BUILD_DIR="$SDL_DIR/build"
 
 #mkdir -p "$BUILD_DIR"
@@ -25,11 +25,11 @@ fi
 mkdir -p "$INSTALL_DIR"
 
 if [[ "$OS_NAME" == "Darwin" ]]; then
-    cp -vf "$BUILD_DIR"/libSDL3.dylib "$INSTALL_DIR"/
+    cp -vf "$BUILD_DIR"/libSDL3.dylib "$INSTALL_DIR"/libSDL3.0.dylib
 elif [[ "$OS_NAME" == "MINGW"* || "$OS_NAME" == "MSYS"* || "$OS_NAME" == "CYGWIN"* ]]; then
     cp -vf "$BUILD_DIR"/Debug/SDL3.dll "$INSTALL_DIR"/
 else
-    cp -vf "$BUILD_DIR"/libSDL3.dylib "$INSTALL_DIR"/
+    cp -vf "$BUILD_DIR"/libSDL3.dylib "$INSTALL_DIR"/libSDL3.0.dylib
 fi
 
 echo "SDL3 build & copy complete: $INSTALL_DIR"
